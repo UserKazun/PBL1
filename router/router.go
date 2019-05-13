dc9fa601e41ee4f98312f2d4346ebdc51385cc3d
尾崎仁一 <kd1192061@st.kobedenshi.ac.jp>
2019-05-13 14:23:49 +0900

    [add] スプリント1までのURIの追加

--- router/router.go
+++ router/router.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"github.com/PBL1/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func apiRouter(api *gin.RouterGroup) {
+	// ログイン時にクッキーにログイン情報を保持させる
+	api.POST("/auth/login", controller.PostLoginDataInCookie)
+
+	// ログアウト時にクッキーのログイン情報を削除する
+	api.POST("/auth/logout", controller.PostLogoutDeleteCookie)
+
+	// メニューIDを受け取り、レシピのURLを返す
+	api.GET("/recipe/:menu_id", controller.GetRecipeByMenuID)
+}
