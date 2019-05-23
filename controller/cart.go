c4c8e21eafeb78992cce028ffcf3fc4fbec411ae
尾崎仁一 <kd1192061@st.kobedenshi.ac.jp>
2019-05-23 13:04:13 +0900

    [add] カートテーブルに商品となる材料のデータを追加するAPIの実装

--- controller/cart.go
+++ controller/cart.go
@@ -1,21 +1,58 @@
 package controller
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/PBL1/service"
 	"github.com/gin-gonic/gin"
 )
 
 // PostIngredientsToCart ...受け取った材料IDを指定されたユーザーのカートに追加する
 func PostIngredientsToCart(c *gin.Context) {
-	userID := c.PostForm("user_id")
 
-	err := AuthCheck(c, userID)
-	if err != nil {
+	// Cart ...カート
+	// type Cart struct {
+	// 	UserID   string `sql:"type:varchar(50)" gorm:"primary_key"`
+	// 	RecipeID uint   `sql:"type:int" gorm:"primary_key"`
+	// 	FoodID   uint
+	// 	Quantity uint
+	// 	Unit     string
+	// }
+
+	// Ingredient ...材料
+	// type Ingredient struct {
+	// 	RecipeID uint `sql:"type:int" gorm:"primary_key"`
+	// 	FoodID   uint `sql:"type:int" gorm:"primary_key"`
+	// 	Quantity uint
+	// 	Unit     string
+	// }
+
+	userID := c.PostForm("user_id")
+	recipeID, _ := strconv.Atoi(c.PostForm("recipe_id"))
+	uintRecipeID := uint(recipeID)
+
+	errCode := AuthCheck(c, userID)
+	if errCode != nil {
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
 
+	modelIngredients, err := service.GetIngredientsByRecipeID(uintRecipeID)
+	if err != nil {
+		log.Println("存在しないレシピIDです")
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	err = service.PostIngredientsToCart(userID, modelIngredients)
+	if err != nil {
+		log.Println("与えられたパラメータが適切ではありません")
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
 	c.AbortWithStatus(http.StatusOK)
-	//c.JSON(http.StatusOK)
+
 }
