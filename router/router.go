a98b4bf79c4b4cb911cbed6def450074004e54ba
尾崎仁一 <kd1192061@st.kobedenshi.ac.jp>
2019-06-11 13:29:06 +0900

    [update] コンフリ解消のための一時的な変更

--- router/router.go
+++ router/router.go
@@ -1,9 +1,9 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/cors"
 )
 
 // GetRouter ...apiGroupやmiddlewareの設定
@@ -11,11 +11,11 @@ func GetRouter() *gin.Engine {
 	r := gin.Default()
 	store := sessions.NewCookieStore([]byte("secret"))
 	r.Use(sessions.Sessions("SessionName", store))
+
 	r.Use(cors.Default())
-	
+
 	api := r.Group("/api/v1")
-	api2 := r.Group("/api/v2")
-	apiRouter(api, api2)
+	apiRouter(api)
 
 	return r
 }
