d8f18b825a937e62b8ddd7d18ab4215f22bbf56d
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2019-06-11 13:23:22 +0900

    [update]router.goの編集

--- router/router.go
+++ router/router.go
@@ -3,7 +3,6 @@ package router
 import (
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"time"
 	"github.com/gin-contrib/cors"
 )
 
@@ -12,21 +11,11 @@ func GetRouter() *gin.Engine {
 	r := gin.Default()
 	store := sessions.NewCookieStore([]byte("secret"))
 	r.Use(sessions.Sessions("SessionName", store))
-
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+	r.Use(cors.Default())
 
 	api := r.Group("/api/v1")
-	apiRouter(api)
+	api2 := r.Group("/api/v2")
+	apiRouter(api, api2)
 
 	return r
 }
